ba56fc71fcee87a569cc5dc63c3f24cb86dd4f76
eamon3481 <68339352+eamon3481@users.noreply.github.com>
2020-08-25 23:24:23 +0900

    save the Username

--- input.js
+++ input.js
@@ -4,6 +4,22 @@ greeting = document.querySelector(".js-greetings")
 
 const USER_LS = "currentUser",
 SHOWING_CN = "showing";
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue=input.value;
+    saveName(currentValue);
+    paintGreeting(currentValue);
+}
+
+
+function askName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
 
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN)
@@ -14,7 +30,7 @@ function paintGreeting(text) {
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
-
+        askName();
     } else {
         paintGreeting(currentUser);
     }
